7d3fd4123e7671de8e821293942411971ac5e783
Marius Matei <mariusmatei31@gmail.com>
2016-12-01 17:22:40 +0200

    open settings in its own window

--- js/main.js
+++ js/main.js
@@ -529,6 +529,10 @@ $(document).ready(function(){
     var displaySelectorsView = new DisplaySelectorView();
     displaySelectorsView.render();
     $('.toggle').toggles();
+    $("#settings_window").click(function(){
+            nw.Window.open('settings.html', {width: 800, height: 600, title:'settings'}, function(win){
+            });
+    });
     $("#playAvi").click(function(el){
             if($(this).data('toggles').active) {
                     localStorage.setItem('playAviSong', 1)
